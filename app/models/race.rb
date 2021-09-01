e1e2809b1497101074f0da538ed8b275ae19b4ff
martin visca <martin.visca@gmail.com>
2021-09-01 23:53:48 +0200

    Add validations

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,2 +1,6 @@
 class Race < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 25}
+  validates :length, presence: true, numericality: { only_integer: true }
+  validates :meet_point, presence: true
+  validates :race_datetime, presence: true
 end
