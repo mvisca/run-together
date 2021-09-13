09d2b875d3a1571e7f9d45c8b2780e69a7baeeb0
martin visca <martin.visca@gmail.com>
2021-09-13 21:06:27 +0200

    add name to user model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,29 +1,9 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:home, :contact]
+
   def home
-    @now = Time.now.strftime("%I:%M %p, %d de %B, %Y.")
-    # Generate Time string with https://strftimer.com/
   end
 
-  def about
-  end
 
-  def contact
-  end
-
-  def new_race
-  end
-
-  def race_details
-    @data = params
-  end
-
-  # def searching
-  #     search = paramas[:name]
-  #     @restuls = if search.present? ? params[:name] : 'Not found'
-  # end
-
-  private
-
-  def get_params
-  end
 end
