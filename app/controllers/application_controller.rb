cf118cdd79f95efb721191e369f46ec99eafdd1d
martin visca <martin.visca@gmail.com>
2021-09-18 19:07:59 +0200

    csrf added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
+  protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # TODO: add new instance variables that will be required in de account creation.
@@ -12,4 +12,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
+  private
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    profile_index_path
+  end
+
+  def after_edit_path_for(resource_or_scope)
+    new_race_path
+  end
 end
