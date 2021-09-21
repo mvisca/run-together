62b114d673814e4ead565c6ef000f43b466aa26f
martin visca <martin.visca@gmail.com>
2021-09-21 23:41:17 +0200

    add cloudinary, active stirage, update user model with photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-
+  has_one_attached :photo
   has_many :runners, dependent: :destroy
   has_many :races, dependent: :destroy
 end
