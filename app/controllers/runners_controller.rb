6787c078e0a52193cdaf8898a8287cf9e8eb73f2
martin visca <martin.visca@gmail.com>
2021-09-21 20:35:10 +0200

    updated race show to hide join button when joined or owner

--- app/controllers/runners_controller.rb
+++ app/controllers/runners_controller.rb
@@ -2,7 +2,7 @@ class RunnersController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @race = Race.find(runner_params)
+    @race = Race.find(params[:race_id])
     if user_signed_in?
       user_id = current_user.id
       race_id = @race.id
@@ -12,12 +12,6 @@ class RunnersController < ApplicationController
     else
       redirect_to new_user_session
     end
-  end
-
-  private
-
-  def runner_params
-    params.require(:runner).permit(:race_id)
   end
 
 end
