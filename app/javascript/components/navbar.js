5f99af505330591c05a24cca443888f9d96c76af
martin visca <martin.visca@gmail.com>
2021-09-22 17:11:08 +0200

    adjust navbar elements align

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,11 +1,11 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar-lewagon');
+  const navbar = document.querySelector('.navbar-rt');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-lewagon-white');
+        navbar.classList.add('navbar-rt-white');
       } else {
-        navbar.classList.remove('navbar-lewagon-white');
+        navbar.classList.remove('navbar-rt-white');
       }
     });
   }
