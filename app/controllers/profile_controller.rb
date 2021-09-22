0fecc8e97723a824efa5176657af63900dfbb4e5
martin visca <martin.visca@gmail.com>
2021-09-22 16:43:50 +0200

    create view/profile/index. create method in profile controller to set owned_races and choice_races

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,5 +2,21 @@ class ProfileController < ApplicationController
   def index
     @user = current_user
     @this_race = Race.new
+    @owned_races = Race.where(user_id: @user.id)
+    @choice_races = set_choice_races
   end
+
+  private
+
+  def set_choice_races
+    runner_opted_in = Runner.where(user_id: current_user.id)
+    choice_races = []
+    runner_opted_in.each do |runner|
+      race = Race.find(runner.race_id)
+      choice_races << race unless race.user_id == current_user.id
+    end
+    choice_races
+  end
+
 end
+
