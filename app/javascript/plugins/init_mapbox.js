1eaa3b95ebb1097b23ba2d615914725b8d55982e
mvisca <martin.visca@gmail.com>
2021-10-13 20:37:42 +0200

    fix partials

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,7 +5,7 @@ const buildMap = (mapElement) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/martinvisca/cku372hl00dmg17p5452yj7h5',
+    style: 'mapbox://styles/mapbox/streets-v10',
     center: [-74.5, 40], // starting position [lng, lat]
   });
 };
