0a65c5eec7cb1a05d1a24fa523df04cbb14cfa1c
mvisca <martin.visca@gmail.com>
2021-10-21 18:14:02 +0200

    progress in profile view. create method to hide email in profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,6 +4,8 @@ class ProfileController < ApplicationController
     @my_races = Race.where(user_id: @user.id)
     @optin_races = set_choice_races
     @race = Race.new
+    @full_name = set_full_name
+    @hidden_email = hide_email
   end
 
   private
@@ -18,5 +20,36 @@ class ProfileController < ApplicationController
     choice_races
   end
 
+  def set_full_name
+    @user
+      .name
+      .split(' ')
+      .map { |name_part| name_part.capitalize }
+      .join(' ')
+  end
+
+  def hide_email
+    email = @user.email.split('@')
+    email_name = met1(email[0])
+    email_domain = met1(email[1])
+    "#{email_name}@#{email_domain}"
+  end
+
+  def met1(string_rt)
+    string_rt
+      .split('.')
+      .map { |word| hide_chars(word) }
+      .join('.')
+  end
+
+  def hide_chars(string)
+    result = "*" * string.length
+    i = -2
+    2.times do
+      result[i] = string.chars[i] if string.length > i * -1
+      i += 1
+    end
+    result
+  end
 end
 
