98c00f27374654ad489ca25799fdcdc2a2d4bb1e
mvisca <martin.visca@gmail.com>
2021-10-24 17:09:56 +0200

    fix index map show

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,12 +6,12 @@ const webpack = require('webpack');
 environment.loaders.delete('nodeModules');
 
 // Bootstrap 4 has a dependency over jQuery & Popper.js:
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
+// environment.plugins.prepend('Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     jQuery: 'jquery',
+//     Popper: ['popper.js', 'default']
+//   })
 );
 
 module.exports = environment
