53a16b8fe63566c8dc5d0f52bfbfdabc90c9e64f
martin <martin.visca@gmail.com>
2025-12-17 17:19:44 +0100

    decoupled docker/dev native/prod

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,14 @@
-# config/puma.rb
-
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# ÚNICA configuración de puerto - ELIMINAR la línea `port` separada
 environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# Workers
+# Workers solo si está definido WEB_CONCURRENCY > 0
 workers_count = ENV.fetch("WEB_CONCURRENCY") { 0 }.to_i
-
 if workers_count > 0
   workers workers_count
   preload_app!
@@ -26,7 +22,7 @@ if workers_count > 0
   end
 end
 
-# ÚNICO binding - usar solo esto
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT') { 3000 }}"
+# Binding universal
+port ENV.fetch("PORT") { 3000 }
 
 plugin :tmp_restart
