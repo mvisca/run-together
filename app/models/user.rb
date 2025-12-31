41a6f9a892aa773d0e1ed9430785ebcffffada77
martin <martin.visca@gmail.com>
2025-12-31 19:13:48 +0100

    fix: implementd coludinary to manage vairants, add gravity face to avatar management

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-     :recoverable, :rememberable, :validatable
+   :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
   has_one_attached :photo
@@ -10,11 +10,25 @@ class User < ApplicationRecord
   has_many :races, dependent: :destroy
   has_one :intro, dependent: :destroy
 
-  def avatar(size: 150)
-  retrurn unless photo.attached?
+  def avatar_url(size: 150)
+    return nil unless photo.attached?
 
-  photo.variant(
-    resize_to_fill: [size, size]
-  )
+	begin
+		env_folder = Rails.env.to_s
+		blob_key = self.photo.blob.key
+
+		return Cloudinary::Utils.cloudinary_url(
+			"#{env_folder}/#{blob_key}",
+			transformation: [
+				{ width: size,
+				height: size,
+				crop: :fill,
+				gravity: :face }
+			]
+		)
+	rescue => err 
+		Rails.logger.error("Error generating avatar_url for user #{id}: #{err.message}")
+		nil
+	end
   end
 end
